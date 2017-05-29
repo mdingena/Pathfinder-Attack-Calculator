e7f624a4e592bf0fd9e5c452d9dc3279c6b14ecf
mdingena <marc.dingena@gmail.com>
2017-05-29 17:08:00 +0200

    Add attack bonus modifiers

--- imports/App.jsx
+++ imports/App.jsx
@@ -10,6 +10,17 @@ const Character = {
 	}
 };
 
+const Modifiers = {
+	featRapidShot : {
+		attackBonus   : -2,
+		sequenceBonus : 1
+	},
+	featDeadlyAim : {
+		attackBonus : -1 * ( 1 + Math.floor( Character.modifier.baseAttackBonus / 4 ) ),
+		damageBonus :  2 * ( 1 + Math.floor( Character.modifier.baseAttackBonus / 4 ) )
+	}
+};
+
 export default class App extends Component {
 	constructor( props ) {
 		super( props );
@@ -26,14 +37,26 @@ export default class App extends Component {
 		});
 	}
 	
+	modifiedAttackBonus() {
+		let attackBonus = 0;
+		for( var key in this.state.configuration ) {
+			if( this.state.configuration.hasOwnProperty( key ) && key.match( /^(feat|buff)/ ) && this.state.configuration[ key ] && Modifiers.hasOwnProperty( key ) && Modifiers[ key ].hasOwnProperty( 'attackBonus' ) ) {
+				attackBonus = attackBonus + Modifiers[ key ].attackBonus;
+			}
+		}
+		return attackBonus;
+	}
+	
 	attackSequence() {
 		const numberOfAttacks = 'fullAttack' === ( this.state.configuration.actionType || 'fullAttack' ) ? 1 + Math.floor( ( Character.modifier.baseAttackBonus - 1 ) / 5 ) : 1;
+		const modifiedAttackBonus = this.modifiedAttackBonus();
 		let attackSequence = [];
 		for( let count = 0; count < numberOfAttacks; ++count ) {
 			const attack = {
 				key         : count,
 				attackBonus : Character.modifier.baseAttackBonus
 							  + Character.modifier.dexterity
+							  + modifiedAttackBonus
 							  - 5 * count
 			};
 			attackSequence.push( attack );
@@ -42,14 +65,13 @@ export default class App extends Component {
 	}
 	
 	render() {
-		const attackSequence = this.attackSequence();
 		return (
 			<div className="app">
 				<Configuration updateCalculator={ this.configurationUpdated } />
 				<ul className="attackSequence">
-					{ attackSequence.map(
+					{ this.attackSequence().map(
 						( attack ) =>
-							<li key={ attack.key }>attackBonus: { attack.attackBonus }</li>
+							<li key={ attack.key }>+{ attack.attackBonus }</li>
 					) }
 				</ul>
 			</div>
